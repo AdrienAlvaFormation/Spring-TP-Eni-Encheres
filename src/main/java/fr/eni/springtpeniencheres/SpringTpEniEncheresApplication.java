947a466ccf5d2cbb2e9db02fa38001788b5d9606
aalvarez2022 <adrien.alvarez.vanhard@gmail.com>
2022-08-22 10:19:22 +0200

    Initial commit

---
+++ src/main/java/fr/eni/springtpeniencheres/SpringTpEniEncheresApplication.java
@@ -0,0 +1,13 @@
+package fr.eni.springtpeniencheres;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringTpEniEncheresApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringTpEniEncheresApplication.class, args);
+    }
+
+}
---
+++ src/test/java/fr/eni/springtpeniencheres/SpringTpEniEncheresApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.eni.springtpeniencheres;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringTpEniEncheresApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
